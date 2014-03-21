026f99592ca44fdfd90eebcf4c1e3f6947341d65
k02 <k02@k201.(none)>
2014-03-21 19:26:48 -0400

    Start inventory application
      - rails new
    
      - Add therubyracer to Gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Inventory::Application.config.secret_key_base = 'e5f2359c2f9055a036c8f530116499c5a4913aaaf6d7d92c6ce1dd291ddcc1d96103cb833697fd2bd75eede0a39d9106bc724dc753bb4c2d1788a37778e6325d'

44922add0c3058d18d6d10376078168587d34519
k02 <you@example.com>
2015-07-19 00:12:28 -0400

    Update routing
      - device
      - cabinet

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Inventory::Application.routes.draw do
+  resources :devices
+
+  resources :cabinets
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
